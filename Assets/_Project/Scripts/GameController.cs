ea82fc0e09e46a5ff6bf683cda076285f74aecd1
Kirk <kirk@spyboy.com>
2020-07-12 20:10:05 -0400

    audio & skybox
    fixed issue w/skybox where there was a line in the sky
    added crane audio (but not linked up yet)
    started on scripts for teleporting between start area and inside of cab

---
+++ Assets/_Project/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GameController : Singleton<GameController>
+
+{
+    public GameObject locationStart;
+    public GameObject locationCab;
+    public Transform rigLocation;
+    
+    public void GotoCab()
+    {
+        /*rigLocation.position=new Vector3(locationCab);*/
+        
+    }
+
+    public void GotoStart()
+    {
+        /*rigLocation.position=new Vector3(locationStart);*/
+    }
+
+}
+
